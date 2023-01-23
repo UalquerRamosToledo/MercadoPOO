ae5e96b46defd31900cdbe35c77ca54f31fe63eb
UalquerRamosToledo <ualquertoledo.aluno@unipampa.edu.br>
2023-01-23 20:56:08 -0300

    Alterando

--- src/main.java
+++ src/main.java
@@ -1,5 +1,12 @@
+import Colecoes.NotasFiscaisColecao;
+import Colecoes.ProdutosColecao;
+import Fonte.NotaFiscal;
+
 public class main {
     public static void main(String args[]){
+        NotasFiscaisColecao notasFiscaisColecao = new NotasFiscaisColecao();
+        ProdutosColecao produtosColecao = new ProdutosColecao();
 
+        System.out.println("");
     }
 }
