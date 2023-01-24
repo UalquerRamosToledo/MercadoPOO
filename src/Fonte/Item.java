5dff9859735e40771f808ab894cd118d9ee4d068
deividfurquim <deividfurquim.aluno@gmail.com>
2023-01-23 21:16:38 -0300

    add Item.java

--- src/Fonte/Item.java
+++ src/Fonte/Item.java
@@ -1,5 +1,47 @@
 package Fonte;
 
 public class Item {
+    private int quantidade;
+    private Produto produto;
+    private double valorUnitario;
+    private double valorTotal;
 
+    public Item(Produto produto, int quantidade) {
+        this.quantidade = quantidade;
+        this.produto = produto;
+        this.valorUnitario = 0;
+        this.valorTotal = 0;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
