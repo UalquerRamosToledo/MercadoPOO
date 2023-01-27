4dd7319b484d78f589a99ac7ae1d830c0af2d8bf
UalquerRamosToledo <ualquertoledo.aluno@unipampa.edu.br>
2023-01-27 20:19:52 -0300

    PDV

--- src/InterfacesGUI/MercadoFrame.java
+++ src/InterfacesGUI/MercadoFrame.java
@@ -4,47 +4,64 @@ import Colecoes.RelacaoDeNotasFiscais;
 import Colecoes.RelacaoDeProdutos;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentListener;
 
 public class MercadoFrame extends JFrame {
     RelacaoDeProdutos relacaoDeProdutos;
     RelacaoDeNotasFiscais relacaoDeNotasFiscais;
+    JMenuBar menuBar;
+    JMenu vendaMenu, estoqueMenu, nfMenu;
+    JMenuItem pdvOption, adicionarProdutoOption, adicionarNFOption;
+    JPanel vendaPanel;
     public MercadoFrame(RelacaoDeProdutos relacaoDeProdutos, RelacaoDeNotasFiscais relacaoDeNotasFiscais){
-
         this.relacaoDeProdutos = relacaoDeProdutos;
         this.relacaoDeNotasFiscais = relacaoDeNotasFiscais;
 
         // Cria um menu bar
-        JMenuBar menuBar = new JMenuBar();
+        menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
         // Cria os menus "Venda, Estoque, Nota Fiscal"
-        JMenu vendaMenu = new JMenu("Venda");
-        JMenu estoqueMenu = new JMenu("Estoque");
-        JMenu nfMenu = new JMenu("Nota Fiscal");
+        vendaMenu = new JMenu("Venda");
+        estoqueMenu = new JMenu("Estoque");
+        nfMenu = new JMenu("Nota Fiscal");
         menuBar.add(vendaMenu);
         menuBar.add(estoqueMenu);
         menuBar.add(nfMenu);
 
 
         // Cria as opções "PDV, exibir Todos" no menu "Venda"
-        JMenuItem pdvOption = new JMenuItem("PDV");
+        pdvOption = new JMenuItem("PDV");
 
         vendaMenu.add(pdvOption);
 
         //Cria as opões de "Adicionar Produto" no menu "Estoque"
-        JMenuItem adicionarProdutoOption = new JMenuItem("Adicionar Produto");
+        adicionarProdutoOption = new JMenuItem("Adicionar Produto");
 
         estoqueMenu.add(adicionarProdutoOption);
 
         //Cria as Opções de "Adicionar" no menu "Nota Fiscal"
 
-        JMenuItem adicionarNFOption = new JMenuItem("Adicionar Nota Fiscal");
+        adicionarNFOption = new JMenuItem("Adicionar Nota Fiscal");
         nfMenu.add(adicionarNFOption);
 
+        add(vendaMenu);
         setTitle("Mercado POO");
         setSize(1200, 800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
         setVisible(true);
+    }
+    public void adicionarListner(){
+        pdvOption.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
