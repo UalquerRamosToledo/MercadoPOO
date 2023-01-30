13a4fe73d02613c9245d5c36756f37dd279a4f7b
deividfurquim <deividfurquim.aluno@gmail.com>
2023-01-30 19:54:58 -0300

    add ExibirNotasFiscaisPanel

---
+++ src/GUI/ExibirNotasFiscaisPanel.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import Colecoes.RelacaoDeNotasFiscais;
+import Colecoes.RelacaoDeProdutos;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ExibirNotasFiscaisPanel extends JPanel{
+
+    public ExibirNotasFiscaisPanel(RelacaoDeNotasFiscais relacaoDeNotasFiscais, RelacaoDeProdutos relacaoDeProdutos){
+        this.setBackground(Color.red);
+    }
+}
