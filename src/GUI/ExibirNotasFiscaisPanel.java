bad08d3a6dc457298ec23fa7dee35ddc8f2d925a
UalquerRamosToledo <ualquertoledo.aluno@unipampa.edu.br>
2023-02-05 23:45:45 -0300

    javadoc 4

--- src/GUI/ExibirNotasFiscaisPanel.java
+++ src/GUI/ExibirNotasFiscaisPanel.java
@@ -5,13 +5,22 @@ import Colecoes.RelacaoDeProdutos;
 
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ * Esta é a classe para a Interface Grafica para Exibir as Notas Fiscais.
+ *
+ * @author Deivid, Ualquer
+ * @version 1.0
+ */
 public class ExibirNotasFiscaisPanel extends JPanel{
-
-    public ExibirNotasFiscaisPanel(RelacaoDeNotasFiscais relacaoDeNotasFiscais, RelacaoDeProdutos relacaoDeProdutos){
-        this.setBackground(Color.red);
-        setVisible(true);
-
-
+    RelacaoDeNotasFiscais relacaoDeNotasFiscais;
+    public ExibirNotasFiscaisPanel(RelacaoDeNotasFiscais relacaoDeNotasFiscais){
+        this.relacaoDeNotasFiscais = relacaoDeNotasFiscais;
+        JTextArea textArea = new JTextArea(relacaoDeNotasFiscais.toString());
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        scrollPane.setPreferredSize(new Dimension(700, 500));
+        JOptionPane.showMessageDialog(null, scrollPane, "Lista de Notas Fiscais", JOptionPane.INFORMATION_MESSAGE);
     }
 }
